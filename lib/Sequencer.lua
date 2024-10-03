9ee1da1e1b7cea1deba986ecb2c081b69b7c0078
256k <fayek.helmi@gmail.com>
2024-10-02 22:42:47 -0400

    updated a bunch of shit. seems semi stable now

--- lib/Sequencer.lua
+++ lib/Sequencer.lua
@@ -1,240 +1,190 @@
 Sequencer = {}
 
 function Sequencer:new(id, noteArray, length, prob, cutoff)
-  print("new Sequencer created")
-  tab.print(noteArray)
-  local s = setmetatable({}, {
-    __index = Sequencer
-  })
-  s.id = id
-  s.seq = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 }
-  s.noteArray = noteArray
-  s.noteIndex = id
-  s.oct = 36
-  s.note = s.noteArray[s.noteIndex]
-  s.length = length
-  s.prob = prob
-  s.step = 0
-  s.cutoff = cutoff
-  s.release = 0.3
-  s.play = true
-  s.tempoDiv = 4
+	local s = setmetatable({}, {
+		__index = Sequencer,
+	})
+	s.id = id
+	s.seq = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 }
+	s.noteArray = noteArray
+	s.noteIndex = id
+	s.oct = 57
+	s.note = noteArray[s.noteIndex]
+	s.length = length
+	s.prob = prob
+	s.step = 0
+	s.cutoff = cutoff
+	s.release = 0.3
+	s.play = true
+	s.tempoDiv = 4
 
-  -- params:
+	-- params:
+	params:add_group("Track " .. id, 5)
+	-------------------------------
 
-  params:add_group("Track " .. id, 5)
-  -------------------------------
+	-- ControlSpec.new (min, max, warp, step, default, units, quantum, wrap)
 
-  -- ControlSpec.new (min, max, warp, step, default, units, quantum, wrap)
+	params:add({
+		type = "control",
+		id = "cutoff" .. id,
+		name = "Cutoff",
+		controlspec = controlspec.new(50, 5000, "exp", 0, 800, "hz"),
+		action = function(x)
+			s.cutoff = x
+		end,
+	})
+	params:add({
+		type = "option",
+		id = "timeDiv" .. id,
+		name = "Time div",
+		default = 4,
+		options = { 1, 2, 3, 4, 6, 8, 16, 32 },
+		action = function(x)
+			s.tempoDiv = x
+		end,
+	})
+	params:add({
+		type = "option",
+		id = "note" .. id,
+		name = "Note",
+		options = s.noteArray,
+		action = function(x)
+			s.note = s.noteArray[x]
+		end,
+		default = s.noteArray[s.noteIndex],
+	})
+	params:add({
+		type = "option",
+		id = "octave" .. id,
+		name = "octave",
+		default = 5,
+		options = { 1, 2, 3, 4, 5, 6, 7, 8 },
+		action = function(x)
+			s.oct = x * 12
+		end,
+	})
 
-  params:add {
-    type = "control",
-    id = "cutoff" .. id,
-    name = "Cutoff",
-    controlspec = controlspec.new(50, 5000, 'exp', 0, 800, 'hz'),
-    action = function(x)
-      s.cutoff = x
-    end
-  }
-  params:add {
-    type = "option",
-    id = "timeDiv" .. id,
-    name = "Time div",
-    default = 4,
-    options = { 1, 2, 3, 4, 6, 8, 16, 32 },
-    action = function(x)
-      s.tempoDiv = x
-    end
-  }
-  params:add {
-    type = "option",
-    id = "note" .. id,
-    name = "Note",
-    options = s.noteArray,
-    action = function(x)
-      s.note = s.noteArray[x]
-    end,
-    default = s.noteArray[s.noteIndex]
-  }
-  params:add {
-    type = "option",
-    id = "octave" .. id,
-    name = "octave",
-    default = 3,
-    options = { 1, 2, 3, 4, 5, 6, 7, 8 },
-    action = function(x)
-      s.oct = x * 12
-    end
-  }
- 
-  -- tab.print(musicutil.SCALES)
+	s_release = controlspec.new(0.05, 3, "lin", 0, 0.5, "s")
+	params:add({
+		type = "control",
+		id = "release" .. id,
+		controlspec = s_release,
+		action = function(x)
+			s.release = x
+			print(x)
+		end,
+	})
 
-  s_release = controlspec.new(0.05, 3, 'lin', 0, 0.5, 's')
-  params:add { type = "control", id = "release" .. id, controlspec = s_release,
-    action = function(x) s.release = x print(x) end }
-  
-  
-  
-  --   s_note = controlspec.new(1,8,'lin',1,1)
-  -- params:add{type="control",id="note"..id,controlspec=s_note,
-  --   action=function(x) s.note=s.noteArray[x] print(x) end}
-  --   s_div = controlspec.new(1,8,'lin',1,1)
-  -- params:add{type="control",id="division"..id,name="time div",controlspec=s_div,
-  -- action=function(x) s.tempoDiv = 2^x end}
-  -- print(s.noteArray[s.noteIndex])
-  
-  
-  s.note = params:get("note" .. id)
-  print("s.note" .. s.note)
-  return s
+	return s
 end
 
 function Sequencer:lengthChange(d)
-  s.length = util.clamp(s.length + d, 1, SEQUENCER_LENGTH)
+	s.length = util.clamp(s.length + d, 1, SEQUENCER_LENGTH)
 end
 
 function Sequencer:lengthChange(d)
-  s.prob = util.clamp(s.prob + d, 1, 100)
+	s.prob = util.clamp(s.prob + d, 1, 100)
 end
 
 function Sequencer:randomize()
-  for i = 1, #self.seq do
-    self.seq[i] = math.random(1, 100);
-  end
-  -- tab.print(self.seq)
+	for i = 1, #self.seq do
+		self.seq[i] = math.random(1, 100)
+	end
 end
 
 function Sequencer:play()
-  self.play = true
+	self.play = true
 end
 
 function Sequencer:stop()
-  self.play = false
+	self.play = false
 end
 
--- function Sequencer:draw_track()
---   print("DRAW TRACK")
---   -- tab.print(self)
---   -- if self then
---     -- tab.print(self)
---     local startx = 10
---     local starty = 40
---     local blockw = 8
---     local blockh = 8
---     local offset = 15
---     step = 1
---     for i = 1, SEQUENCER_LENGTH do
---       if i == SEQUENCER_LENGTH/2 + 1 then
---         step = 1
---         starty = starty + startx + 2
---       end
---       stepLevel = map(self.seq[i], 0, 100, 15, 1)
---       screen.level(stepLevel)
---       screen.rect( offset + startx*step, starty, blockw, blockw)
---       if i > self.length then screen.level(0) end
---       screen.fill()
---       if i == self.step then screen.level(15) else screen.level(0) end
---       if i > self.length then screen.level(0) end
---       screen.rect(offset + startx*step, starty, blockw, blockw)
---       screen.stroke()
---       screen.level(15)
---       step = step + 1
---     end
--- end
-
 function Sequencer:draw_track_linear()
+	print("track draw linear trigggered track: ")
+	local startx = 6
+	local starty = 40
+	local blockw = 4
+	local blockh = 8
+	local offset = 4
+	local xMargin = 10
+	step = 1
+	screen.blend_mode(1)
+	for i = 1, SEQUENCER_LENGTH do
+		if i == SEQUENCER_LENGTH + 1 then
+			i = 1
+		end
 
-    local startx = 6
-    local starty = 40
-    local blockw = 4
-    local blockh = 8
-    local offset = 4
-    local xMargin = 10
-    step = 1
-    screen.blend_mode(1)
-    for i = 1, SEQUENCER_LENGTH do
-      -- if i == SEQUENCER_LENGTH + 1 then
-      --   step = 1
-      --   starty = starty + startx + 2
-      -- end
-      if i == SEQUENCER_LENGTH + 1 then
-        i = 1  
-      end
-      
-      stepLevel = map(self.seq[i], 0, 100, 20 , 1)
-      screen.level(15)
-      
-      -- this is the step value block 
-      screen.rect(xMargin + offset + startx*step, starty + 20, blockw, -stepLevel)
-      if i > self.length then screen.level(0) end
-      screen.fill()
-      
-      if i == self.step then screen.level(15) else screen.level(0) end
-      if i > self.length then screen.level(0) end
-      
-      
-      -- this is the step indicator stoke
-      screen.rect(xMargin + offset + startx*step, starty + 20, blockw, -stepLevel)
-      screen.stroke()
-      screen.level(15)
-      step = step + 1
-    end
-    
-    -- this is the probability line
-    screen.blend_mode(0)
-    local probMapped = map(self.prob,0, 100, 0, 20)
-    -- print("probability:" .. probMapped)
-    screen.rect(xMargin + offset + startx - (blockw / 2), starty + probMapped - 1, (blockw + 2 )* SEQUENCER_LENGTH, 1)
-    -------------------------------
+		stepLevel = map(self.seq[i], 0, 100, 20, 1)
+		screen.level(15)
+
+		-- this is the step value block
+		screen.rect(xMargin + offset + startx * step, starty + 20, blockw, -stepLevel)
+		if i > self.length then
+			screen.level(0)
+		end
+		screen.fill()
+
+		if i == self.step then
+			screen.level(15)
+		else
+			screen.level(0)
+		end
+		if i > self.length then
+			screen.level(0)
+		end
+
+		-- this is the step indicator stoke
+		screen.rect(xMargin + offset + startx * step, starty + 20, blockw, -stepLevel)
+		screen.stroke()
+		screen.level(15)
+		step = step + 1
+	end
+
+	-- this is the probability line
+	screen.blend_mode(0)
+	local probMapped = map(self.prob, 0, 100, 0, 20)
+	screen.rect(xMargin + offset + startx - (blockw / 2), starty + probMapped - 1, (blockw + 2) * SEQUENCER_LENGTH, 1)
+	-------------------------------
 end
 
 function Sequencer:trigger()
-
-  -- checks probability for bang or no bang
-  self:inc_step()
-  if self.seq[self.step] < self.prob then
-    -- print("test" .. self.noteArray[self.noteIndex])
-    local notefreq = MusicUtil.note_num_to_freq(self.note + self.oct)
-    self:setEngine()
-    engine.hz(notefreq)
-    -- local player = params:lookup_param("voice"):get_player()
-    -- player:note_off(self.note)
-    -- player:note_on(self.note, 1)
-        
-  else
-    -- skip step
-  end
-  
-  -- self:draw_track()
+	-- checks probability for bang or no bang
+	self:inc_step()
+	if self.seq[self.step] < self.prob then
+		local notefreq = MusicUtil.note_num_to_freq(self.note + self.oct)
+		print("note: ", notefreq, self.note, self.oct)
+		self:setEngine()
+		engine.hz(notefreq)
+	else
+		-- skip step
+	end
 end
 
 function Sequencer:setEngine()
-  print("set engine")
-  engine.cutoff(self.cutoff)
-  engine.release(self.release)
+	-- this is to update the engine with the cutoff and release values
+	engine.cutoff(self.cutoff)
+	engine.release(self.release)
 end
 
 function Sequencer:run()
-  clock.run(function()
-    while true do
-      clock.sync(1 / self.tempoDiv)
-      self:trigger()
-      if menu_page == self.id then 
-        redraw()
-        
-      end
-      
-    end
-  end)
+	clock.run(function()
+		while true do
+			clock.sync(1 / self.tempoDiv)
+			self:trigger()
+			if menu_page == self.id then
+			end
+		end
+	end)
 end
 
 function Sequencer:inc_step()
-  -- increases the step advance and resets to 1 if over the length
-  if self.step + 1 <= self.length then
-    self.step = self.step + 1
-  else
-    self.step = 1
-  end
+	-- increases the step advance and resets to 1 if over the length
+	if self.step + 1 <= self.length then
+		self.step = self.step + 1
+	else
+		self.step = 1
+	end
 end
 
 return Sequencer
--- prolly.lua
+++ prolly.lua
@@ -1,19 +1,19 @@
 -- prolly
--- a port of the euro module
+-- a looping rando-probabilistic sequencer
 -- by 256k
-hs = include('lib/halfsecond')
-local nb = require "nb/lib/nb"
+
+
+engine.name = "PolyPerc"
+HALFSECOND = include('lib/halfsecond')
 MusicUtil = require("musicutil")
-sequins = require 'sequins'
 SEQUENCER_LENGTH = 16
 
 -- import local sequencer object
 local Sequencer = include('lib/Sequencer')
 
-notes_num = MusicUtil.generate_scale(1,"dorian")
+notes_num = MusicUtil.generate_scale(3,"dorian",3)
 
 
--- notes_freq = MusicUtil.note_nums_to_freqs(notes_num)
 
 
 local seq_count = 16
@@ -22,101 +22,79 @@ local seqs = {}
 
 menu_page = 1
 
-engine.name = "PolyPerc"
 
 local SCALE_NAMES = {}
 
 for i=1,#MusicUtil.SCALES do
-  SCALE_NAMES[i] = MusicUtil.SCALES[i].name
+   SCALE_NAMES[i] = MusicUtil.SCALES[i].name
 end
 
-tab.print(SCALE_NAMES)
 
 function init()
-nb:init()
-    nb:add_param("voice", "voice")
-    nb:add_player_params()
-  params:add{
-    type= "option",
-    id = "scale_selection",
-    name = "Scale",
-    options=SCALE_NAMES,
-    action = function(x) 
-      notes_num = MusicUtil.generate_scale(57,x)
-      end
-  }
+   HALFSECOND.init()
+   engine.release(0.33)
+   engine.cutoff(600)
+   
+   params:add_separator(" ")
+   params:add_separator("prolly settings")
+   for i=1,seq_count do
+      -- sequencer(id, notearray, length, probability, cutoff)
+      seqs[i] = Sequencer:new(i,notes_num, 16, 0, 800)
+   end
 
-
-
-  params:add_separator("tracks")
-  for i=1,seq_count do
-    -- sequencer(id, notearray, length, probability)
-seqs[i] = Sequencer:new(i,notes_num, 16, 0, 800)
+   for i=1,#seqs do
+      seqs[i]:randomize()
+      seqs[i]:run()
+   end
 end
 
-  engine.release(0.33)
-  engine.cutoff(600)
-  for i=1,#seqs do
-  seqs[i]:randomize()
-  seqs[i]:run()
-  end
-
-
-  clock.run(function()  -- redraw the screen 
-    while true do
-      clock.sleep(1/10)
-      -- redraw()
-    end
-  end)
-  
+function refresh()
+   redraw()
 end
-
-hs.init()
 
 function redraw()
-  local y_space = 8
-screen.clear()
-screen.move(4,y_space*1)
-screen.text("Length: ".. seqs[menu_page].length)
-screen.move(4,y_space*2)
-screen.text("probability: ".. seqs[menu_page].prob)
-screen.move(4,y_space*3)
-local note_name = MusicUtil.note_num_to_name(seqs[menu_page].note + seqs[menu_page].oct, true) 
--- print(note_name)
-screen.text("note: "..note_name )
-screen.move(90, 10)
-screen.font_size(16)
-screen.text("[ "..menu_page.." ]")
-screen.font_size(8)
-seqs[menu_page]:draw_track_linear()
-
-screen.update()  
+   local y_space = 8
+   screen.clear()
+   screen.move(4,y_space*1)
+   screen.text("Length: ".. seqs[menu_page].length)
+   screen.move(4,y_space*2)
+   screen.text("probability: ".. seqs[menu_page].prob)
+   screen.move(4,y_space*3)
+   local note_name = MusicUtil.note_num_to_name(seqs[menu_page].note + seqs[menu_page].oct, true) 
+   screen.text("note: "..note_name )
+   screen.move(90, 10)
+   screen.font_size(16)
+   screen.text("[ "..menu_page.." ]")
+   screen.font_size(8)
+   seqs[menu_page]:draw_track_linear()
+   
+   screen.update()  
 end
 
 function key(k,z)
-if k==1 and z== 1 then 
-  seqs[menu_page]:randomize()
-  screen.clear()
-  screen.move(35,28)
-  screen.text("REGEN")
-  screen.update()
-  redraw()
-  end
+   if k==1 and z== 1 then 
+      seqs[menu_page]:randomize()
+      screen.clear()
+      screen.move(35,28)
+      screen.text("REGEN")
+      screen.update()
+      redraw()
+   end
 end
 
 function enc(n,d)
-  -- changing pages between diff sequencers
-  if n==1 then menu_page = util.clamp(menu_page + d, 1,seq_count) end
-  if n==2 then seqs[menu_page].length = util.clamp(seqs[menu_page].length + d, 1, 16)  end
-  if n==3 then seqs[menu_page].prob = util.clamp(seqs[menu_page].prob + d, 0, 100)  end
-  redraw()
+   -- changing pages between diff sequencers
+   if n==1 then menu_page = util.clamp(menu_page + d, 1,seq_count) end
+   if n==2 then seqs[menu_page].length = util.clamp(seqs[menu_page].length + d, 1, 16)  end
+   if n==3 then seqs[menu_page].prob = util.clamp(seqs[menu_page].prob + d, 0, 100)  end
+   redraw()
 end
 
 function map(OldValue, OldMin, OldMax, NewMin, NewMax)
-  OldRange = (OldMax - OldMin)  
-  NewRange = (NewMax - NewMin)  
-  NewValue = (((OldValue - OldMin) * NewRange) / OldRange) + NewMin
-  return math.floor(NewValue)
+   OldRange = (OldMax - OldMin)  
+   NewRange = (NewMax - NewMin)  
+   NewValue = (((OldValue - OldMin) * NewRange) / OldRange) + NewMin
+   return math.floor(NewValue)
 end
 
 
